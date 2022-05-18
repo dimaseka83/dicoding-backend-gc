e230b743b33af276e3698d792d62ca76425548a6
dhimaseka <dimaseka83@gmail.com>
2022-05-18 14:07:31 +0700

    ++

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-const Tiger = require('./Tiger'); 
-const Wolf = require('./Wolf');
+const {EventEmitter} = require('events');
 
-const fighting = (tiger, wolf) => {
-    if(tiger.strength > wolf.strength){
-        tiger.growl();
-        return;
-    }
-
-    if(wolf.strength > tiger.strength){
-        wolf.howl();
-        return
-    }
-
-    console.log('Tiger and Wolf have the same strength');
+const birthdayEvenListener = (name) => {
+    console.log(`Happy Birthday ${name}!`);
 }
 
-const tiger = new Tiger();
-const wolf = new Wolf();
+const myEmitter = new EventEmitter();
 
-fighting(tiger, wolf);
+myEmitter.on('birthday', birthdayEvenListener);
+
+myEmitter.emit('birthday', 'John');
