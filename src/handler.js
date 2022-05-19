69ad2524425dd1adc3be53e06c7297da6050b37e
dhimaseka <dimaseka83@gmail.com>
2022-05-19 16:52:47 +0700

    ++

---
+++ src/handler.js
@@ -0,0 +1,100 @@
+const { nanoid } = require("nanoid");
+const notes = require("./notes");
+
+const addNoteHandler = (request, h) => {
+    const {title, tags, body} = request.payload;
+
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const newNote = {
+        title, tags, body, id, createdAt, updatedAt
+    };
+
+    notes.push(newNote);
+
+    const isSuccess = notes.filter(note => note.id === id).length > 0;
+
+    if (isSuccess) {
+        const response = h.response({
+            status: "success",
+            message: "Note added successfully",
+            data: {
+                noteId: id
+            }
+        });
+        response.code(201);
+        return response;
+    }
+
+    const response = h.response({
+        status : 'fail',
+        message: 'Note could not be added'
+    });
+    response.code(500);
+    return response;
+}
+
+const getAllNotesHandler = () => ({
+    status: 'success',
+    data: {
+        notes
+    }
+});
+
+const getNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+
+    const note = notes.filter((n) => n.id === id)[0];
+    
+    if (note) {
+        return {
+            status: 'success',
+            data: note
+        };
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Note not found'
+    });
+    response.code(404);
+    return response;
+}
+
+const editNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+
+    const { title, tags, body } = request.payload;
+    const updatedAt = new Date().toISOString();
+
+    const index = notes.findIndex((n) => n.id === id);
+
+    if (index !== -1){
+        notes[index] = {
+            ...notes[index],
+            title,
+            tags,
+            body,
+            updatedAt
+        };
+
+        const response = h.response({
+            status: 'success',
+            message: 'Note updated successfully',
+        });
+
+        response.code(200);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Note not found'
+    });
+    response.code(404);
+    return response;
+}
+
+module.exports = {addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler};
--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
 
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: 'localhost'
+        host: 'localhost',
+        routes: {
+            cors: {
+                origin: ['*'],
+            }
+        }
     });
-
-    server.route(routes)
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
