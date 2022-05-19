d9f2d010f3f3f7be25644800c65cdeae85d5e46a
dhimaseka <dimaseka83@gmail.com>
2022-05-19 13:48:40 +0700

    ++

--- server.js
+++ server.js
@@ -4,24 +4,50 @@ const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
 
-    const {method} = request;
+    const {method, url} = request;
 
-    if (method === 'GET') {
-        response.end('<h1>Hello World</h1>');
+    if (url === '/') {
+        if(method === 'GET') {
+            response.end('<h1>Ini adalah homepage</h1>');
+        }else{
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+        }
+    }else if (url === '/about') {
+        if (method === 'GET') {
+            response.end('<h1>Ini adalah halaman about</h1>');
+        }else if(method === 'POST') {
+            let body = [];
+
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const {name} = JSON.parse(body);
+                response.end(`<h1>${name}</h1>`);
+            })
+        }
+    }else{
+        response.end('<h1>404 Halaman tidak ditemukan</h1>');
     }
 
-    if (method === 'POST') {
-        let body = [];
+    // if (method === 'GET') {
+    //     response.end('<h1>Hello World</h1>');
+    // }
 
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
+    // if (method === 'POST') {
+    //     let body = [];
 
-        request.on('end', () => {
-            body = Buffer.concat(body).toString();
-            response.end(`<h1>Hello ${body}</h1>`);
-        })
-    }
+    //     request.on('data', (chunk) => {
+    //         body.push(chunk);
+    //     });
+
+    //     request.on('end', () => {
+    //         body = Buffer.concat(body).toString();
+    //         response.end(`<h1>Hello ${body}</h1>`);
+    //     })
+    // }
 }
 
 const server = http.createServer(requestListener);
