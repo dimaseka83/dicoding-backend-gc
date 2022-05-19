57a4152bbd3277c6f71a57898ad734447f4acde3
dhimaseka <dimaseka83@gmail.com>
2022-05-19 14:32:20 +0700

    ++

--- server.js
+++ server.js
@@ -9,15 +9,21 @@ const requestListener = (request, response) => {
     if (url === '/') {
         if(method === 'GET') {
             response.statusCode = 200;
-            response.end('<h1>Ini adalah homepage</h1>');
+            response.end(JSON.stringify({
+                message: 'Ini adalah homepage'
+            }))
         }else{
             response.statusCode = 400;
-            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan method ${method}`
+            }))
         }
     }else if (url === '/about') {
         if (method === 'GET') {
             response.statusCode = 200;
-            response.end('<h1>Ini adalah halaman about</h1>');
+            response.end(JSON.stringify({
+                message: 'Halo, ini halaman about'
+            }))
         }else if(method === 'POST') {
             let body = [];
 
@@ -29,15 +35,21 @@ const requestListener = (request, response) => {
                 body = Buffer.concat(body).toString();
                 const {name} = JSON.parse(body);
                 response.statusCode = 200;
-                response.end(`<h1>${name}</h1>`);
+                response.end(JSON.stringify({
+                    message: `Halo, ini halaman about, nama saya ${name}`
+                }));
             })
         }else{
             response.statusCode = 400;
-            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan method ${method}`
+            }));
         }
     }else{
         response.statusCode = 404;
-        response.end('<h1>404 Halaman tidak ditemukan</h1>');
+        response.end(JSON.stringify({
+            message: 'Halaman tidak ditemukan'
+        }));
     }
 
     // if (method === 'GET') {
